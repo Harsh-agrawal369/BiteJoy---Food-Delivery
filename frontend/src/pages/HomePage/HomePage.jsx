d50f90c784599687614b3d406613a224ae74cf84
Harsh-agrawal369 <agrawalharsh831@gmail.com>
2024-06-26 00:00:17 +0530

    Basic Explore-menu Structure

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const HomePage = () => {
   return (
     <div>
       <Header/>
+      <ExploreMenu/>
     </div>
   )
 }
