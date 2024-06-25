a7d5ad6c5b55aa05adf54a2a1c216af6e02eaf99
Harsh-agrawal369 <agrawalharsh831@gmail.com>
2024-06-25 19:07:17 +0530

    Routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart'
+import HomePage from './pages/HomePage/HomePage'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/placeorder' element={<PlaceOrder/>} />
+      </Routes>
     </div>
   )
 }
