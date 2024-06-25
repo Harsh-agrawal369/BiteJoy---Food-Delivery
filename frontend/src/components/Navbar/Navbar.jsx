4d3f0a7c6f7218d7b341625dbe73bd69ef361038
Harsh-agrawal369 <agrawalharsh831@gmail.com>
2024-06-25 18:48:31 +0530

    first commit

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+const Navbar = () => {
+
+  const [menu, setMenu] = useState("home");
+
+
+  return (
+    <div className='navbar'>
+      <img src={assets.biteJoy_logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li onClick={() => setMenu("home")} className={menu === "home" ? "active":""}>Home</li>
+        <li onClick={() => setMenu("menu")} className={menu === "menu"? "active" : ""}>Menu</li>
+        <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app"? "active" : ""}>Mobile-app</li>
+        <li onClick={() => setMenu("contact-us")} className={menu === "contact-us"? "active" : ""}>Contact Us</li>
+      </ul>
+
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" className="search" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon}></img>
+          <div className="dot"></div>
+        </div>
+        <button>Sign-In</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
