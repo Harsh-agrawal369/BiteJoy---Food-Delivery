4bf0e6fbedb4f9c313d8ea65d9eb43526c9d8b8a
Harsh-agrawal369 <agrawalharsh831@gmail.com>
2024-06-27 18:06:40 +0530

    Food display with filtering done

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -17,8 +17,7 @@ const ExploreMenu = ({ category, setCategory }) => {
           return (
             <div
               onClick={() =>
-                setCategory(prev=>prev === item.menu_name ? "All" : item.menu_name
-                )
+                setCategory(prev=>prev === item.menu_name ? "All" : item.menu_name)
               }
               key={index}
               className="exlore-menu-list-item"
