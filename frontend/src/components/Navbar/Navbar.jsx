ddcebdd42f7cdc467f297d2b9111a04e7420db3d
Harsh-agrawal369 <agrawalharsh831@gmail.com>
2024-07-04 21:16:34 +0530

    Cart page layout created, functionality remaning

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({showLogin, setShowLogin}) => {
 
   return (
     <div className='navbar'>
-      <img src={assets.biteJoy_logo} alt="" className="logo" />
+      <Link to='/' ><img src={assets.biteJoy_logo} alt="" className="logo" /></Link> 
       <ul className="navbar-menu">
         <Link to='/' onClick={() => setMenu("home")} className={menu === "home" ? "active":""}>Home</Link>
         <a  href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu"? "active" : ""}>Menu</a>
@@ -21,7 +21,7 @@ const Navbar = ({showLogin, setShowLogin}) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" className="search" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon}></img>
+          <Link to='/Cart'><img src={assets.basket_icon}></img></Link> 
           <div className="dot"></div>
         </div>
         <button onClick={() => {setShowLogin(true)}}>Sign-In</button>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,95 @@
-import React from 'react'
-import './Cart.css'
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../Context/StoreContext";
+import { assets } from "../../assets/assets";
 
 const Cart = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { cartItems, food_list, addToCart, removeFromCart } =
+    useContext(StoreContext);
 
-export default Cart
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Add Item</p>
+        </div>
+        <br />
+        <hr />
+
+        {food_list.map((item) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <React.Fragment key={item._id}>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt={item.name} />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <div className="food-item-counter">
+                    <img
+                      onClick={() => removeFromCart(item._id)}
+                      src={assets.remove_icon_red}
+                      alt="Remove"
+                    />
+                    <p>{cartItems[item._id]}</p>
+                    <img
+                      onClick={() => addToCart(item._id)}
+                      src={assets.add_icon_green}
+                      alt="Add"
+                    />
+                  </div>
+                  <p  onClick={() => removeFromCart(item._id)} className="cross">x</p>
+                </div>
+                <hr />
+              </React.Fragment>
+            );
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr />
+
+            <div className="cart-total-details">
+              <p>Delivery fee</p>
+              <p>{2}</p>
+            </div>
+            <hr />
+
+            <div className="cart-total-details">
+              <p>Total</p>
+              <p>{0}</p>
+            </div>
+          </div>
+
+          <button>Proceed to Checkout</button>
+        </div>
+
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a Promocode, Enter it here</p>
+
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="Promocode" />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
